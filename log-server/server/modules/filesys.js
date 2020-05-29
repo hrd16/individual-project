a2684efef08cf2e6885b59a0c120a406006a1ea2
Harry Davis <ib.harrydavis@gmail.com>
2020-05-29 07:54:23 +0100

    Add file sys logging for messages

--- log-server/server/modules/filesys.js
+++ log-server/server/modules/filesys.js
@@ -3,19 +3,27 @@ const fs = require('fs');
 module.exports = function (handler) {
     let streams = new Map();
 
-    function getFileForNode(node) {
-        if (streams.has(node)) {
-            return streams.get(node);
+    function getFileForNode(node, type) {
+        let key = `${node}-${type}`;
+        if (streams.has(key)) {
+            return streams.get(key);
         } else {
-            var file = fs.createWriteStream(`/var/simul/${node}.txt`, {flags:'a'});
-            streams.set(node, file);
+            var file = fs.createWriteStream(`/var/simul/${key}.txt`, {flags:'a'});
+            streams.set(key, file);
             return file;
         }
     }
 
     handler.subscribe('app', msgs => { 
         msgs.forEach(msg => {
-            let file = getFileForNode(msg.node);
+            let file = getFileForNode(msg.node, 'node');
+            file.write(JSON.stringify(msg) + "\n");
+        });
+    });
+
+    handler.subscribe('proxy', msgs => { 
+        msgs.forEach(msg => {
+            let file = getFileForNode(msg.to, 'messages');
             file.write(JSON.stringify(msg) + "\n");
         });
     });
